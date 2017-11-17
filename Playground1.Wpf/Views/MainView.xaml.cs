b033954f9ef6e463090ab00b98a69fa5950a7edd
jz5 <jz5@hotmail.com>
2017-11-17 15:42:10 +0900

    プロジェクト ファイルを追加します。

---
+++ Playground1.Wpf/Views/MainView.xaml.cs
@@ -0,0 +1,12 @@
+using MvvmCross.Wpf.Views;
+
+namespace Playground1.Wpf.Views
+{
+    public partial class MainView : MvxWpfView
+    {
+        public MainView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
